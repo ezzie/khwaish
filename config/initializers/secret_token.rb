c707811801229d7d2076f1019c75a541ae4dbc2c
Rajan Punchouty <rajan@ezzie.in>
2013-01-23 02:31:59 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Khwaish::Application.config.secret_token = '383eb1fea2fa8a4e71e471978c87609247f38bb0d7115dca9ace586bf49f69c7bd65efc9fe847be7d30b5bd8b75ec8e3d9532dbc28551d430dd968daf799b532'

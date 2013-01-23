8dcb013f399bac9971faec220ccf830d60c30172
Rajan Punchouty <rajan@ezzie.in>
2013-01-23 15:40:42 +0530

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  subject { page }
+  let(:base_title) { "Place holder" }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: "#{base_title} | Sign Up") }
+  end
+end
